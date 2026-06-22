9ba2569862a6596a854efcc9ab1a6c2ab7f06116
JangiliBunny <jangilibunny2@gmail.com>
2026-06-22 18:24:28 +0530

    answer evaluation and interview report

--- backend/controllers/answerController.js
+++ backend/controllers/answerController.js
@@ -1,4 +1,5 @@
 const Answer= require("../models/Answer");
+const Interview=require("../models/Interview");
 
 const createAnswer= async (req, res)=>{
     try{
@@ -52,7 +53,85 @@ const getAnswerByInterview= async(req, res)=>{
             message:"internal server error",
         })
     }
+};
+
+const evaluateAnswer= async(req, res)=>{
+    try{
+        const answer=await Answer.findById(req.params.id);
+
+        if(!answer){
+            return res.status(404).json({
+                success:false,
+                message:"answer not found",
+            });
+        }
+
+        answer.score=8;
+        answer.feedback="good explanation , its optimal solution with hashmap";
+
+        answer.save();
+
+        return res.status(200).json({
+            success:true,
+            message:"answer evaluated",
+            answer,
+        });
+    }catch(err){
+        console.log(err);
+        return res.status(500).json({
+            success:false,
+            message:"internal server error",
+        });
+    }
+};
+
+const getInterviewReport= async(req, res)=>{
+    try{
+        const interviewId=req.params.id;
+
+        const interview=await Interview.findById(interviewId);
+
+        if(!interview){
+            return res.status(404).json({
+                success:false,
+                message:"interview not found",
+            });
+        }
+
+        const answers=await Answer.find({interview:interviewId});
+
+        const totalQuestions=interview.questions.length;
+
+        const totalAnswers=answers.length;
+
+        let totalScore=0;
+
+        for(let answer of answers){
+            totalScore+=answer.score;
+        }
+
+        let averageScore= answers.length > 0 ? totalScore/answers.length : 0;
+
+        return res.status(200).json({
+            success:true,
+            report:{
+                totalQuestions,
+                totalAnswers,
+                averageScore,
+                status:interview.status,
+            }
+        })
+        
+    }catch(err){
+        console.log(err);
+        return res.status(500).json({
+            success:false,
+            message:"internal server error",
+        });
+    }
 }
 
 
-module.exports={createAnswer , getAnswerByInterview};
+module.exports={createAnswer , getAnswerByInterview,
+                evaluateAnswer, getInterviewReport
+};
--- backend/routes/answerRoutes.js
+++ backend/routes/answerRoutes.js
@@ -1,12 +1,17 @@
 const express=require("express");
 const router=express.Router();
 
-const {createAnswer, getAnswerByInterview}=require("../controllers/answerController");
+const {createAnswer, getAnswerByInterview,
+        evaluateAnswer, getInterviewReport}=require("../controllers/answerController");
 const auth=require("../middlewares/authMiddleware");
 
 
 router.post("/", auth, createAnswer);
 
 router.get("/interview/:interviewId", auth, getAnswerByInterview);
+
+router.post("/:id/evaluate", auth , evaluateAnswer);
+
+router.get("/report/:id", auth, getInterviewReport);
 
 module.exports=router;
