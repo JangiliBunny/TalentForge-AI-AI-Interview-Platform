8b67336d0746924fa42180716f4da142e6097ed6
JangiliBunny <jangilibunny2@gmail.com>
2026-06-24 18:33:41 +0530

    added frontend folder structue

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+
+function App(){
+  return (
+    <BrowserRouter> 
+    <Routes>
+       <Route>
+           path="/login"
+          element={<Login />}
+       </Route>
+
+       <Route>
+           path="/register"
+          element={<Register />}
+       </Route>
+       <Route>
+           path="/dashboard"
+          element={<Dashboard />}
+       </Route>
+
+
+    </Routes>
+    </BrowserRouter>
+  )
+
+}
+
+export default App;
