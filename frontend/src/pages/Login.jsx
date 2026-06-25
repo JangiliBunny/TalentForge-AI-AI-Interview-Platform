e3dd73d611da965857adb3e7c266980df7b8db23
JangiliBunny <jangilibunny2@gmail.com>
2026-06-25 17:44:28 +0530

    login and register page created

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,88 @@
-function Login() {
-  return <h1>Login Page</h1>;
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../services/api";
+import '../App.css';
+
+function Login(){
+    const navigate=useNavigate();
+
+    const [formData, setFormData]=useState({
+       email:"",
+       password:""
+    });
+
+    const handleChange=(e)=>{
+      setFormData({
+        ...formData,
+        [e.target.name]:e.target.value,
+      });
+    }
+
+    const handleSubmit=async(e)=>{
+        e.preventDefault();
+         try{
+              const res=await api.post(
+                "/auth/login",
+                formData,
+              );
+              alert(res.data.message);
+              navigate("/dashboard");
+         }catch (err) {
+
+            console.log(err);
+
+            alert(
+                err.response?.data?.message ||
+                "Registration Failed"
+            );
+
+        }
+    }
+
+    return (
+        <div className="login-container">
+            <div className="login-card">
+              <h1>TalentForge</h1>
+
+                <h2>Create Account</h2>
+                 
+                 <form onSubmit={handleSubmit}>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="What's your e-mail?"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Your password?"
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                    />
+
+                    <button type="submit">
+                        Login
+                    </button>
+                 </form>
+                 <p>
+
+                    Don't have an account?
+
+
+                    <Link to="/register">
+                        Sign up
+                    </Link>
+
+                </p>
+            </div>
+
+        </div>
+    )
 }
 
 export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,126 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../services/api";
+import '../App.css';
+
 function Register() {
-  return <h1>Register Page</h1>;
+
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        role: "student"
+    });
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+
+            const res = await api.post(
+                "/auth/register",
+                formData
+            );
+
+            alert(res.data.message);
+
+            navigate("/login");
+
+        } catch (err) {
+
+            console.log(err);
+
+            alert(
+                err.response?.data?.message ||
+                "Registration Failed"
+            );
+
+        }
+    };
+
+    return (
+        <div className="register-container">
+
+            <div className="register-card">
+
+                <h1>TalentForge</h1>
+
+                <h2>Create Account</h2>
+
+                <form onSubmit={handleSubmit}>
+
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="What's your Full Name?"
+                        value={formData.name}
+                        onChange={handleChange}
+                        required
+                    />
+
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="What`s Your Email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Your password?"
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                    />
+
+                    <select
+                        name="role"
+                        value={formData.role}
+                        onChange={handleChange}
+                    >
+
+                        <option value="student">
+                            Student
+                        </option>
+
+                        <option value="admin">
+                            Admin
+                        </option>
+
+                    </select>
+
+                    <button type="submit">
+                        Register
+                    </button>
+
+                </form>
+
+                <p>
+
+                    Already have an account?
+
+                    <Link to="/login">
+                        Login
+                    </Link>
+
+                </p>
+
+            </div>
+
+        </div>
+    );
 }
 
 export default Register;
